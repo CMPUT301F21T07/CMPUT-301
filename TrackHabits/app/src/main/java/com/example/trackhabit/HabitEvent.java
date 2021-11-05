7d1dba15215e9f5db37e5b84d2c06104fd9f8399
Yerbol <smailov008@gmail.com>
2021-11-04 22:32:13 -0600

    Modified ManageHabitEventsFragment and HabitEvent classes

--- TrackHabits/app/src/main/java/com/example/trackhabit/HabitEvent.java
+++ TrackHabits/app/src/main/java/com/example/trackhabit/HabitEvent.java
@@ -4,34 +4,42 @@ import android.graphics.Bitmap;
 import android.widget.Switch;
 
 public class HabitEvent {
-  
-    private Habits habit;
+    private String habitName;
+    private String userName;
     private String date;
     private String comment;
     private Bitmap optionalPhoto;
     private boolean locationPermission;
 
     public HabitEvent(Habits habit, String date) {
-        this.habit = habit;
+        this.habitName = habit.getHabitName();
+        this.userName = habit.getHabitUser();
         this.date = date;
     }
 
     public HabitEvent(Habits habit, String date, String comment, Bitmap photo, boolean locationPermission) {
-
-        this.habit = habit;
+        this.habitName = habit.getHabitName();
+        this.userName = habit.getHabitUser();
         this.date = date;
         this.comment = comment;
         this.optionalPhoto = photo;
         this.locationPermission = locationPermission;
     }
 
-    public Habits getHabit() {
-        return habit;
+    public String getHabitName() {
+        return habitName;
     }
 
-    public void setHabit(Habits habit) {
+    public void setHabitName(String habitName) {
+        this.habitName = habitName;
+    }
 
-        this.habit = habit;
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     public String getDate() {
