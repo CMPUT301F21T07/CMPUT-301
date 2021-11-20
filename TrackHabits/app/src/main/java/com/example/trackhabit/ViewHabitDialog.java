687111ed07e0ba692c0f080c777755074eeb791a
AMooseAteSagan <67478355+AMooseAteSagan@users.noreply.github.com>
2021-11-20 13:54:51 -0700

    Updated View Habits
    Now contains all details and consistency measure. Measure still needs to be completed, currently only considers the amount of events that should have happened this week, not since the start date

--- TrackHabits/app/src/main/java/com/example/trackhabit/ViewHabitDialog.java
+++ TrackHabits/app/src/main/java/com/example/trackhabit/ViewHabitDialog.java
@@ -1,23 +1,36 @@
 package com.example.trackhabit;
 
+import static java.lang.String.valueOf;
+
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 public class ViewHabitDialog extends AppCompatDialogFragment {
-    private TextView habitNameView, habitTitleView, habitReasonView, habitStartDateView, habitPrivacyView, habitDaysView;
+    private TextView habitNameView, habitTitleView, habitReasonView, habitStartDateView, habitPrivacyView, habitDaysView, habitConsistView;
     private String userName, habitName, habitTitle, habitStart, habitReason, days;
     private String habitDays = "";
-    private Boolean itemPrivacy; //if privacy setting will be included in the details
+    private Integer habitConsist = 0, amountEvents = 0;
+    private Boolean itemPrivacy;
     private final String[] privacyOptions = new String[]{"Private","Public"};
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference habitEventsRef = db.collection("Habit Events");
 
 
 
@@ -56,6 +69,7 @@ public class ViewHabitDialog extends AppCompatDialogFragment {
         habitStartDateView  = view.findViewById(R.id.view_start_date);
         habitPrivacyView  = view.findViewById(R.id.view_privacy);
         habitDaysView  = view.findViewById(R.id.view_days);
+        habitConsistView = view.findViewById(R.id.view_consist);
 
         //set text views
         habitNameView.setText(habitName);
@@ -69,6 +83,7 @@ public class ViewHabitDialog extends AppCompatDialogFragment {
         } else {
             habitPrivacyView.setText(privacyOptions[1]);
         }
+        getConsistency();
 
         return builder.create();
     }
@@ -92,5 +107,50 @@ public class ViewHabitDialog extends AppCompatDialogFragment {
             habitDays = "None";
         }
     }
+
+    private void getConsistency(){
+
+        habitEventsRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            /**
+             * Function that updates the Firebase database on an event
+             * @param value This is the message that holds any supported value type
+             * @param error This is an error
+             */
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+
+                assert value != null;
+                for(QueryDocumentSnapshot doc: value)
+                {
+                    Log.d("TAG", String.valueOf(doc.getData().get(habitName)));
+                    System.out.println("Checkpoint get for habit Name: "+habitName+", and user Name: "+userName);
+                    String habitNames = (String) doc.getData().get("HabitName");
+                    String userNames = (String) doc.getData().get("UserName");
+                    String date = (String) doc.getData().get("Date");
+
+                    if (userNames.equals(habitName) && habitNames.equals(userName)){ /**This will need to be updated once events is fixed*/
+
+                        amountEvents++;
+                        System.out.println("**!! SUCCESSFUL MATCH !! - habit Name: "+habitNames+",  username: "+userNames+", date: "+date+", # Events: "+amountEvents);
+                    }
+                }
+                Integer nDays = countDays();
+                System.out.println("**!! Calculating consistency with - # days: "+nDays+", # Events: "+amountEvents);
+                if(nDays > 0 && amountEvents > 0){
+                    habitConsist = 100/nDays*amountEvents;}
+                else {habitConsist = 0+100*amountEvents;}
+                habitConsistView.setText(valueOf(habitConsist)+" %");
+
+            }
+        });
+
+    }
+
+    private Integer countDays(){
+        Integer amountDays = days.length();
+        //multiply the days by amount of weeks since start date+days for partial weeks
+        return amountDays;
+    }
+
 
 }
