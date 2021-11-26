4c9613a0cd619e9c6681000cc913988a266fa9a0
Yerbol <smailov008@gmail.com>
2021-11-26 00:04:48 -0700

    Implemented take picture from camera, Implemented image storage.

--- TrackHabits/app/src/main/java/com/example/trackhabit/ManageHabitEventsFragment.java
+++ TrackHabits/app/src/main/java/com/example/trackhabit/ManageHabitEventsFragment.java
@@ -9,6 +9,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -32,7 +33,11 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -46,21 +51,26 @@ public class ManageHabitEventsFragment extends DialogFragment  {
     private ImageView optionalPhoto;
     private ToggleButton locationPermissionButton;
 
-    private HabitEvent editableHabitEvent;
     private String habitName;
     private String userName;
+    private String dataName;
     private String manageType = "Add";
     private String date;
     private String comment;
+    private String location = "";
     private Bitmap photo;
+    private Boolean photoUploaded = false;
     private Boolean locationPermission;
     private EditEventListener listener;
 
     private Boolean isOkPressed = false;
-    private Boolean hasTakenPicture = false;
 
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private CollectionReference habitEventsRef = db.collection("Habit Events");
+    final private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    final private CollectionReference habitEventsRef = db.collection("Habit Events");
+    final private FirebaseStorage storage = FirebaseStorage.getInstance();
+    final private StorageReference storageRef = storage.getReference();
+
+    StorageReference optionalPhotoRef;
 
     public interface EditEventListener {
         void onOkPressed();
@@ -72,13 +82,13 @@ public class ManageHabitEventsFragment extends DialogFragment  {
         this.manageType = manageType;
     }
     public ManageHabitEventsFragment(String habitName, String userName, String manageType,
-                                     String comment, Bitmap photo, Boolean locationPermission,
+                                     String comment, Boolean photoUploaded, Boolean locationPermission,
                                      String date) {
         this.habitName = habitName;
         this.userName = userName;
         this.manageType = manageType;
         this.comment = comment;
-        this.photo = photo;
+        this.photoUploaded = photoUploaded;
         this.locationPermission = locationPermission;
         this.date = date;
     }
@@ -112,10 +122,22 @@ public class ManageHabitEventsFragment extends DialogFragment  {
             commentEditText.setText(comment);
             optionalPhoto.setImageBitmap(photo);
             locationPermissionButton.setChecked(locationPermission);
+            if (photoUploaded) {
+                dataName = habitName + " " + userName + " " + date;
+                optionalPhotoRef = storageRef.child(dataName + ".jpg");
+                final long mb = 1024 * 1024;
+                optionalPhotoRef.getBytes(mb).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        Bitmap photo = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                        optionalPhoto.setImageBitmap(photo);
+                    }
+                });
+            }
             title = "Edit HabitEvent Info";
         }
 
-        String dataName = habitName + " " + userName + " " + date;
+        dataName = habitName + " " + userName + " " + date;
         System.out.println(habitEventsRef.document(dataName).get().toString());
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         return builder
@@ -127,22 +149,38 @@ public class ManageHabitEventsFragment extends DialogFragment  {
                     public void onClick(DialogInterface dialogInterface, int i) {
                         comment = commentEditText.getText().toString();
                         optionalPhoto.setDrawingCacheEnabled(true);
-                        if (!hasTakenPicture) {
-                            photo = optionalPhoto.getDrawingCache();
-                        }
                         locationPermission = locationPermissionButton.isChecked();
 
                         checkInputCorrectness();
                         System.out.println(date);
-                        HabitEvent newHabitEvent = new HabitEvent(habitName, userName, date, comment, photo,
-                                locationPermission);
+
+                        // Storing image to Storage
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        photo.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                        byte[] data = baos.toByteArray();
+                        optionalPhotoRef = storageRef.child(dataName + ".jpg");
+                        UploadTask uploadTask = optionalPhotoRef.putBytes(data);
+                        uploadTask.addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                Log.d("TAG", "Photo Was Not Stored!");
+                            }
+                        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                Log.d("TAG", "Photo Successfully Stored!");
+                            }
+                        });
+
+                        // Storing habit event to Firestore
                         HashMap<String, Object> habitEventData = new HashMap<>();
                         habitEventData.put("HabitName", habitName);
                         habitEventData.put("UserName", userName);
                         habitEventData.put("Date", date);
                         habitEventData.put("OptionalComment", comment);
-                        habitEventData.put("OptionalPhoto", photo);
                         habitEventData.put("LocationPermission", locationPermission);
+                        habitEventData.put("PhotoUploaded", photoUploaded);
+                        habitEventData.put("Location", location);
                         String dataName = habitName + " " + userName + " " + date;
                         habitEventsRef.document(dataName)
                                 .set(habitEventData)
@@ -155,7 +193,7 @@ public class ManageHabitEventsFragment extends DialogFragment  {
                                 .addOnFailureListener(new OnFailureListener() {
                                     @Override
                                     public void onFailure(@NonNull Exception e) {
-                                        Log.d("TAG", "Habit Successfully Added!");
+                                        Log.d("TAG", "Habit Event Was Not Added!");
                                     }
                                 });
                         if (manageType.equals("Edit")) {
@@ -172,9 +210,13 @@ public class ManageHabitEventsFragment extends DialogFragment  {
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101) {
+            Toast.makeText(getContext(), "Permission for Camera is denied!", Toast.LENGTH_SHORT).show();
+        }
         if (requestCode == 100) {
             photo = (Bitmap)data.getExtras().get("data");
             optionalPhoto.setImageBitmap(photo);
+            photoUploaded = true;
         }
     }
 
